0146e2bea01297bb00cc3e47cec31867d79fc4db
Pravin-Choudhary <pravinchoudhary.aesxa29@gmail.com>
2025-10-19 21:17:38 +0530

    added website logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Intellisuite",
-  description: "Create You First Dashbard",
+  description: "Turn Your Data into Decisions — Instantly.",
+  icons: {
+    icon: "/intellisuiteCropLogo.png", // or "/my-logo.png"
+  },
 };
 
 export default function RootLayout({
